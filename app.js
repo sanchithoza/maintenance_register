990eb3a3dcd629a25b931b71f4b892826ca7439d
sanchithoza <sanchithoza@gmail.com>
2021-12-13 19:24:44 +0530

    for heroku again

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fastify = require("./main");
 const port = process.env.PORT || 9000;
 console.log(port);
-fastify.listen(port, (err) => {
+fastify.listen(port, '0.0.0.0', (err) => {
     if (err) throw err;
     console.log(`server listening on ${fastify.server.address().port}`);
 });
