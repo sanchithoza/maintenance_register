1d70afdcb55abecded8060bb51904955da380c9b
sanchithoza <sanchithoza@gmail.com>
2021-12-30 20:21:01 +0530

    working on pdf generation

--- routes/opd.js
+++ routes/opd.js
@@ -1,15 +1,14 @@
 const boom = require('boom');
 const { result } = require('lodash');
+const pdfGenerator = require('pdfkit')
 const knex = require('../knex');
 async function routes(fastify, options) {
     fastify.post('/newEntry',async(request,reply)=>{
-        console.log(request.body);
         knex("tbl_opd_register").insert(request.body).then((result)=>{
-            reply.status(200).send([{"h":"hello"}])
+            reply.status(200).send(result)
         }).catch((error)=>{
             reply.status(400).send(error)
         })
-        
     })  
     fastify.get('/getEntries',async(request,reply)=>{
         await knex("tbl_opd_register").select().then((result)=>{
